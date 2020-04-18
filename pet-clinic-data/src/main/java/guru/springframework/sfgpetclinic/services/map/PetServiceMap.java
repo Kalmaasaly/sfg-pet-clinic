ece02366a036f56d0621adf7565ea972aae554b3
Kalmaasali <kalmaasaly@gmail.com>
2020-04-18 22:01:10 +0800

    Impl Map base Service, Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by @author Kalmaasali on 18/04/2020.
+ **/
+public class PetServiceMap extends AbstractMapService<Pet, Long>
+        implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+        //return save;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
