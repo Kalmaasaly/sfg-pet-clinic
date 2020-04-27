e5e26fe63c0762a80b0234367889830433545834
Kalmaasali <kalmaasaly@gmail.com>
2020-04-28 01:17:09 +0800

    added Speciality and added to Vet. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by @author Kalmaasali on 28/04/2020.
+ **/
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
