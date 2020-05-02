506adf00a19a38df5f2d1f96514a138dfd21b491
Kalmaasali <kalmaasaly@gmail.com>
2020-05-03 02:00:09 +0800

    added spring data jpa service for owners. Closes #41

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by @author Kalmaasali on 03/05/2020.
  **/
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
