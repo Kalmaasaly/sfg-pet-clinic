46c1aa8dff0d0708946b298e80c9cb628ef72ae4
Kalmaasali <kalmaasaly@gmail.com>
2020-05-03 23:34:56 +0800

    Implement Spring Data JPA service for Visit. Closes #49.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -3,12 +3,14 @@ package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.repositories.VisitRepository;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * Created by @author Kalmaasali on 03/05/2020.
  **/
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long>
         implements VisitService {
     private final VisitRepository visitRepository;
